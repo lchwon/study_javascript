90765eaef0f433c4232ae0415cb26657935e06b3
lchwon <wodn1411@gamil.com>
2024-09-03 20:37:14 +0900

    Object

---
+++ javascript_basic/11_copy_by_value_and_reference.js
@@ -0,0 +1,7 @@
+/**
+ * copy by value 값에 의한 전달
+ * copy by reference 참조에 의한 전달
+ * 
+ * 1) 기본적으로 모든 primitive 값은 copy by value다. (privitive = 원시타입)
+ * 2) 객체는 copy by reference다. (reference = 참조타입)
+ */
